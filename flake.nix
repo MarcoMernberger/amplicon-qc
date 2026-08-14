72cc9712e0cd0a43dd64041f0c47ac7d34c1f093
MarcoMernberger <marco.mernberger@gmail.com>
2026-08-14 14:05:14 +0200

    flake

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "amplicon-qc - FASTQ read counter";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+
+      version =
+        if self ? rev
+        then builtins.substring 0 8 self.rev
+        else "dev";
+    in {
+      packages.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "amplicon-qc";
+        inherit version;
+
+        src = self;
+
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
+
+        meta = with pkgs.lib; {
+          description = "FASTQ flank check tool";
+          platforms = platforms.linux;
+        };
+      };
+
+      defaultPackage = self.packages.${system}.default;
+
+      apps.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/amplicon-qc";
+      };
+
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          cargo
+          rustc
+          rustfmt
+          clippy
+          rust-analyzer
+          pkg-config
+        ];
+      };
+    });
+}
